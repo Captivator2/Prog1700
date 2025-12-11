10ee30e78a1873c337a7f1080546802df8a84b04
Vincent <toluafuwape2020@gmail.com>
2025-12-11 14:25:03 -0400

    exam 2

--- alarm-exam-practice/w0505186-alarm-deactivation-final.py
+++ alarm-exam-practice/w0505186-alarm-deactivation-final.py
@@ -13,21 +13,46 @@ print("Password attempts left:", password_attempts)
 with open("passwords.txt", "r") as file:
     valid_codes = [line.strip() for line in file]
 
+print("Loaded valid codes:", valid_codes)
+
+
 # Main 3-attempt loop
 while password_attempts > 0:
 
     user_input = input("Enter your 5-digit password: ")
 
-# Ask the user to enter a 5-digit password
-user_input = input("Enter your 5-digit password: ")
+    if len(user_input) != 5 or not user_input.isdigit():
+        print("Invalid Password")
+        password_attempts -= 1
+        print("Password attempts left:", password_attempts)
+        continue
 
-# Check if the password is exactly 5 characters AND all digits
-if len(user_input) != 5 or not user_input.isdigit():
-    print("Invalid Password")
-    password_attempts -= 1   
-    print("Password attempts left:", password_attempts)
-    # later we will loop back depending on attempts
-else:
     print("Valid number entered.")
 
+    if user_input in valid_codes:
+        print("SYSTEM DEACTIVATED")
+        break
+    else:
+        print("Incorrect Password")
+        password_attempts -= 1
+        print("Password attempts left:", password_attempts)
+        continue
+# If the loop ends because attempts reached 0, trigger alarm
+if password_attempts == 0:
+    print("ALARM TRIGGERED")
 
+# Now user must enter valid password to stop alarm
+    while True:
+        user_input = input("Enter password to disable alarm: ")
+
+        if len(user_input) != 5 or not user_input.isdigit():
+            print("Invalid Password")
+            continue
+
+        if user_input in valid_codes:
+            print("ALARM DISABLED")
+            alarm_activated_status = "OFF"
+            break
+        else:
+            print("Incorrect Password – Alarm still active!")
+    
